5fbee4605d5e093e65adce6ab2525ee91eb173e6
Luis Romo <lromo@iteso.mx>
2021-10-14 01:51:04 +0000

    Latest

---
+++ rick-and-morty/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Rick and Morty</h1>
+    <ListCharacters />
+  </div>
+</template>
+
+<script>
+import ListCharacters from "@/components/ListCharacters";
+export default {
+  name: "App",
+  components: {
+    ListCharacters,
+  },
+};
+</script>
